faed4acd7c3b2619b8552ee010d1aea6f4ac5d6b
Carol Jorge <carol.ajorges@gmail.com>
2023-01-13 18:48:14 -0300

    Projeto com banco

---
+++ Contatos.dpr
@@ -0,0 +1,14 @@
+program Contatos;
+
+uses
+  Vcl.Forms,
+  ufrmContatos in 'ufrmContatos.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
--- readme.txt
+++ readme.txt
@@ -1,2 +1,1 @@
 Projeto criado conforme as ideias do curso de Delphi 10 da Alura.
-Iniciado em 12/01/2023.
